04b192896655e5ee9ee4f90f85a3af72a0694427
Deep Govindvira <deepgovindvira@gmail.com>
2024-07-21 21:06:17 +0530

    Complete SignOut

--- Chat/app/src/main/java/com/example/chat/activities/MainActivity.java
+++ Chat/app/src/main/java/com/example/chat/activities/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.chat.activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -9,6 +14,13 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.chat.databinding.ActivityMainBinding;
 import com.example.chat.utilities.Constants;
 import com.example.chat.utilities.PreferenceManager;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,5 +49,59 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        loaduserDetails();
+        getToken();
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.imageSignOut.setOnClickListener(view -> signOut());
+    }
+
+    private void loaduserDetails() {
+        binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
+        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        binding.imageProfile.setImageBitmap(bitmap);
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void getToken() {
+        FirebaseMessaging.getInstance().getToken().addOnSuccessListener(this::updateToken);
+    }
+
+    private void updateToken(String token) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference documentReference =
+                db.collection(Constants.KEY_COLLECTION_USERS)
+                        .document(
+                                preferenceManager.getString(Constants.KEY_USER_ID)
+                        );
+        documentReference.update(Constants.KEY_FCM_TOKEN, token)
+                .addOnSuccessListener(unused -> showToast("Token updated successfully"))
+                .addOnFailureListener(e -> showToast("Unable to update token"));
+    }
+
+    private void signOut() {
+        showToast("Signing out ...");
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference documentReference =
+                db.collection(Constants.KEY_COLLECTION_USERS)
+                        .document(
+                                preferenceManager.getString(Constants.KEY_USER_ID)
+                        );
+        Map<String, Object> updates = new HashMap<>();
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        documentReference.update(updates)
+                .addOnSuccessListener(unused -> {
+                    preferenceManager.clear();
+                    startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+                    finish();
+                })
+                .addOnFailureListener(e -> showToast("Unable to sign out"));
     }
 }
